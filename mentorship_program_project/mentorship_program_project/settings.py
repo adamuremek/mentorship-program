ed73e635c22b63f3ed5fdee94de9d9e9728949a9
Logan Zipp <loganzipp@yahoo.com>
2024-01-30 19:33:24 -0500

    settings.py file path updates

--- mentorship_program_project/mentorship_program_project/settings.py
+++ mentorship_program_project/mentorship_program_project/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mentorship_program_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
