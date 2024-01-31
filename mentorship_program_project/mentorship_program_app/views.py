a2fa91af32962ad2d188747680b0c022c322d22e
Anthony Peters <108294207+anthonyPeters143@users.noreply.github.com>
2024-01-30 20:18:01 -0500

    role section card and template
    Created a card template and retrofitted html for role section
    
    Set up testing url and view for role_section.html @ http://localhost:8000/test1

--- mentorship_program_project/mentorship_program_app/views.py
+++ mentorship_program_project/mentorship_program_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def test_view1(request):
+    template = loader.get_template('sign-in card/role_selection.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
--- mentorship_program_project/mentorship_program_project/urls.py
+++ mentorship_program_project/mentorship_program_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from mentorship_program_app import views
 
 urlpatterns = [
+    path('test1', views.test_view1, name='test_view1'),
     path('admin/', admin.site.urls),
 ]
