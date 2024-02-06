f4be8b19b7d5b8f0b05935cf0981a13af964a67f
AndrewPyscher <andypyscher@gmail.com>
2024-02-06 15:59:20 -0500

    updated create user function

--- mentorship_program_project/mentorship_program_app/Database/user.py
+++ mentorship_program_project/mentorship_program_app/Database/user.py
@@ -1,26 +1,37 @@
 from models import *
 from datetime import date
+import traceback
 
 def createUser(strEmailAddress: str, strPasswordHash:str, objRole:Users.Role, clsDateJoined:date, clsActiveChangedDate:date, 
                blnActive:bool, blnAccountDisabled:bool, strFirstname:str, strLastName:str, strPhoneNumber:str, 
                clsDateOfBirth:date, strGender:str, strPreferredPronouns:str, strSessionID:str, strSessionKeyHash:str):
+    try:
+        Users.objects.create(
+        clsEmailAddress = strEmailAddress,
+        strPasswordHash = strPasswordHash,
+        strRole = objRole,
+        clsDateJoined = clsDateJoined,
+        clsActiveChangedDate = clsActiveChangedDate,
+        blnActive = blnActive,
+        blnAccountDisabled = blnAccountDisabled,
+        strFirstName = strFirstname,
+        strLastName = strLastName,
+        strPhoneNumber = strPhoneNumber,
+        clsDateofBirth = clsDateOfBirth,
+        strGender = strGender,
+        strPreferredPronouns = strPreferredPronouns,
+        strSessionID = strSessionID,
+        strSessionKeyHash = strSessionKeyHash
+        )
+        return True
+    except Exception as e:
+        traceback.print_exc()
+        return False
+        
+
+    
+
+def updateUser(strFirstName:str, strLastName:str, str):
     user = Users()
-    user.clsEmailAddress = strEmailAddress
-    user.strPasswordHash = strPasswordHash
-    user.strRole = objRole
-    user.clsDateJoined = clsDateJoined
-    user.clsActiveChangedDate = clsActiveChangedDate
-    user.blnActive = blnActive
-    user.blnAccountDisabled = blnAccountDisabled
-    user.strFirstName = strFirstname
-    user.strLastName = strLastName
-    user.strPhoneNumber = strPhoneNumber
-    user.clsDateofBirth = clsDateOfBirth
-    user.strGender = strGender
-    user.strPreferredPronouns = strPreferredPronouns
-    user.strSessionID = strSessionID
-    user.strSessionKeyHash = strSessionKeyHash
-    
-    user.save()
-    
+     
     
