3e5611fd8df72e537eac102d8ca241acfe9aed60
Tanner Williams <tannerawilliams10@gmail.com>
2024-02-06 16:59:58 -0500

    Grabbed some templates and routes from frontend team for testing purposes - Tanner

---
+++ mentorship_program_project/mentorship_program_app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+
+
+##All created urls need ot go here
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('login/success', views.success, name='success'),
+
+    path('', views.default, name='default'),
+    path('landing/', views.landing, name='landing'),
+
+    # TESTING AND DEV ROUTES WILL NEED TO CHECK/REVIEW BEFORE PUBLISHING FROM LOGAN
+    path('role_selection/', views.role_selection, name='role_selection'),
+    path('account_activation_mentee/', views.account_activation_mentee, name='account_activation_mentee'),
+    path('account_activation_invalid/', views.account_activation_invalid_mentee, name='account_activation_invalid'),
+
+]
