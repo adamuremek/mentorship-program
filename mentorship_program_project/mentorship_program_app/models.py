17897343f257910c6937aaf0ed78251e8f8498ab
David Kennamer <dakennam@svsu.edu>
2024-02-08 14:11:58 -0500

    WIP interest functionality

--- mentorship_program_project/mentorship_program_app/models.py
+++ mentorship_program_project/mentorship_program_app/models.py
@@ -7,101 +7,6 @@ from utils import security
 
 # Create your models here.
 
-class Users(Model):
-    """
-
-    """
-    
-    
-    #PLACEHOLDER: Change to User_Accounts
-    #   User_Accounts have a User_Profile
-    class Role(TextChoices):
-        ADMIN = 'Admin'
-        MENTOR = 'Mentor'
-        MENTEE = 'Mentee'
-
-    clsEmailAddress =  EmailField(null=True,unique=True)  
-    strPasswordHash =  CharField(max_length=100, null=True, blank=False)
-    strPasswordSalt =  CharField(max_length=100, null=True, blank=False)
-    strRole = CharField(max_length=10, choices=Role.choices, default='')
-    clsDateJoined = DateField(default=date.today)
-    clsActiveChangedDate = DateField(default=date.today)
-    blnActive = BooleanField(default=True)
-    blnAccountDisabled =  BooleanField(default=False)
-
-    #PLACEHOLDER: Move to User_Profiles
-    strFirstName: CharField =  CharField(max_length=747,null=True)
-    strLastName =  CharField(max_length=747, null=True) 
-    strPhoneNumber =  CharField(max_length=15, null=True)
-    clsDateofBirth = DateField(default=date.today)
-    strGender = CharField(max_length=35, default='')
-    strPreferredPronouns = CharField(max_length=50, null=True)
- 
-    #PLACEHOLDER: Session_Information
-    #   Separate session into its own table or keep with User_Accounts?
-    strSessionID = CharField(max_length=255,default='')
-    strSessionKeyHash = CharField(max_length=100,default='')
-
-    
-    def __str__(self):
-        return self.firstname + ' ' + self.lastname
-    
-    #returns true if the incoming plain text hashes out to our stored
-    #password hash
-    def check_valid_password(self,password_plain_text : str)->bool:
-        return security.hash_password(password,self.strPasswordSalt) ==\
-                self.strPasswordHash
-
-    """
-    
-    Takes in a user id and an array of interest id's and adds the given interest to the 
-    user
-
-    """
-    @staticmethod 
-    def add_interests_to_user(intUserId : int, intInterestIdArray : [int])->bool:
-        for interestId in intInterestIdArray:
-            user_interest_link = User_Interests.objects.create(intUserId,interestId)
-            user_interest_link.save()
-
-    """
-    returns a NON SAVED user object that has the password properly hashed
-    and salt correctly generated it's your responsibility to save this object 
-    if you want it to persist in the db YOU HAVE BEEN WARNED >_>
-    """
-    @staticmethod
-    def create_from_plain_text_and_email(password_plain_text : str,
-                                         email : str)->'Users':
-        generated_user_salt = security.generate_salt()
-        #TODO: emails need to be validated, send a sacrifical lamb
-        #to the regex gods
-        return Users.objects.create(
-                    strPasswordHash = security.hash_password(
-                                            password_plain_text,
-                                            generated_user_salt),
-                    strPasswordSalt = generated_user_salt,
-                    clsEmailAddress = email
-                )
-
-
-
-
-class Biographies(Model):
-    """
-
-    """
-    intUserID = OneToOneField(
-        "Users",
-        on_delete = models.CASCADE,
-        primary_key=True
-    )
-
-    strBio = CharField(max_length=5000, null=True)
-
-    def __str__(self):
-        return self.bio
-
-
 class Interests(Model):
     """
 
@@ -147,6 +52,115 @@ class Interests(Model):
 
     def __str__(self):
         return self.strInterest
+
+
+class Users(Model):
+    """
+
+    """
+    
+    
+    #PLACEHOLDER: Change to User_Accounts
+    #   User_Accounts have a User_Profile
+    class Role(TextChoices):
+        ADMIN = 'Admin'
+        MENTOR = 'Mentor'
+        MENTEE = 'Mentee'
+
+    clsEmailAddress =  EmailField(null=True,unique=True)  
+    strPasswordHash =  CharField(max_length=100, null=True, blank=False)
+    strPasswordSalt =  CharField(max_length=100, null=True, blank=False)
+    strRole = CharField(max_length=10, choices=Role.choices, default='')
+    clsDateJoined = DateField(default=date.today)
+    clsActiveChangedDate = DateField(default=date.today)
+    blnActive = BooleanField(default=True)
+    blnAccountDisabled =  BooleanField(default=False)
+
+    #PLACEHOLDER: Move to User_Profiles
+    strFirstName: CharField =  CharField(max_length=747,null=True)
+    strLastName =  CharField(max_length=747, null=True) 
+    strPhoneNumber =  CharField(max_length=15, null=True)
+    clsDateofBirth = DateField(default=date.today)
+    strGender = CharField(max_length=35, default='')
+    strPreferredPronouns = CharField(max_length=50, null=True)
+ 
+    #PLACEHOLDER: Session_Information
+    #   Separate session into its own table or keep with User_Accounts?
+    strSessionID = CharField(max_length=255,default='')
+    strSessionKeyHash = CharField(max_length=100,default='')
+
+    
+    #def __str__(self):
+    #    return self.firstname + ' ' + self.lastname
+
+    """
+    returns a list of interests that are associated with a given user
+    """
+    def get_interests(self)->[Interests]:
+        #there is probably a more django-eee way to make this work
+        #that doens't involve hitting the database in a for loop, 
+        #backend people please help I have no idea and I must code 0_0
+       
+        #everyone is interested in EVERYTHING huzzah!
+        return Users.objects.all()
+        return [
+                    Interests.objects.get(intInterestID=user_interest_map.intInterestID)
+                for user_interest_map in 
+                    User_Interests.objects.filter(intUserID=self.id)
+                ]
+    
+    #returns true if the incoming plain text hashes out to our stored
+    #password hash
+    def check_valid_password(self,password_plain_text : str)->bool:
+        return security.hash_password(password,self.strPasswordSalt) ==\
+                self.strPasswordHash
+
+    """
+    
+    adds the given interests to the user
+
+    """
+    def add_interests(intUserID : int, intInterestIdArray : [int])->bool:
+        for interestId in intInterestIdArray:
+            user_interest_link = User_Interests.objects.create(intUserID=,intInterestID=Interests.objets.get(intInterestIdArray))
+            user_interest_link.save()
+
+    """
+    returns a NON SAVED user object that has the password properly hashed
+    and salt correctly generated it's your responsibility to save this object 
+    if you want it to persist in the db YOU HAVE BEEN WARNED >_>
+    """
+    @staticmethod
+    def create_from_plain_text_and_email(password_plain_text : str,
+                                         email : str)->'Users':
+        generated_user_salt = security.generate_salt()
+        #TODO: emails need to be validated, send a sacrifical lamb
+        #to the regex gods
+        return Users.objects.create(
+                    strPasswordHash = security.hash_password(
+                                            password_plain_text,
+                                            generated_user_salt),
+                    strPasswordSalt = generated_user_salt,
+                    clsEmailAddress = email
+                )
+
+
+
+
+class Biographies(Model):
+    """
+
+    """
+    intUserID = OneToOneField(
+        "Users",
+        on_delete = models.CASCADE,
+        primary_key=True
+    )
+
+    strBio = CharField(max_length=5000, null=True)
+
+    def __str__(self):
+        return self.bio
 
 
 class User_Interests(Model):
--- mentorship_program_project/utils/development.py
+++ mentorship_program_project/utils/development.py
@@ -23,6 +23,29 @@ def print_debug(*args):
 
 
 """
+returns an array of randomly selected interests of a given size
+"""
+def get_random_interests(size : int)->[Interests]:
+    random_interest_list = []
+    interest_objects = Interests.objects.all()
+
+    #if we have to randomly pick the entire set,
+    #then we return, well the set
+    if size >= len(interest_objects):
+        return interest_objects
+
+
+    for i in range(size):
+        random_choice = random.choice(interest_objects)
+        
+        while random_choice in random_interest_list:
+            random_choice = random.choice(interest_objects)
+        
+        random_interest_list.append(random_choice)
+    
+    return random_interest_list
+
+"""
 fills the database with randomly generated users ONLY if in 
 debug mode
 """
@@ -34,11 +57,19 @@ def populate_database_with_random_users(amount  : int = 100)->None:
 
     for i in range(amount):
         user = f'user{i}@'+''.join(random.choice(string.ascii_letters)for _ in range(100))+'.com'
+
+
         new_user = Users.create_from_plain_text_and_email(
                                                 f'password{i}',
                                                 user
                                                 )
         new_user.save()
+
+        user_interests = get_random_interests(5)
+        Users.add_interests_to_user(new_user.id,user_interests)
+
+        print_debug('[random user generator] ' + str(new_user.get_interests()))
+
         print_debug('[random user generator] finished random user generation!\n\tenjoy the data :) '+user)
 
 """
@@ -52,14 +83,15 @@ is an ok place for it for now
 """
 def populate_database_with_interests()->None:
     for interest in Interests.getDefaultInterestList():
-        if len(Interests.objects.filter(strInterest = interest)) > 0:
+        
+        try:
+            Interests.objects.get(strInterest = interest)
+            print_debug(f"[interest populator] {interest} is already in the database")
+        except:
             interest_object = Interests.objects.create(strInterest = interest)
             interest_object.save()
             
             print_debug(f'[interest populator] finished with {interest}')
-        else:
-            
-            print_debug(f"[interest populator] {interest} is already in the database")
 
 
 
