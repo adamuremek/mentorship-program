04a31e79571bc104c1cdb6274627cb9a6765ad52
Anthony Peters <108294207+anthonyPeters143@users.noreply.github.com>
2024-02-18 18:19:57 -0500

    Added shared dir to the sign-in card template dir. Includes email_password, interests, name_pronoun, user_agreement
    Updated single_page_mentee.html and single_page_mentor.html:
    Updated to use new shared dir and templates
    
    views.py:
    Added useragremeent to THEBIGMOVE view

--- mentorship_program_project/mentorship_program_app/views.py
+++ mentorship_program_project/mentorship_program_app/views.py
@@ -53,6 +53,10 @@ def THEBIGMOVE(req):
             '0 years',
             '0-2 years', 
             '2-5 years'],
+
+        'useragreement': 
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
     }
     return HttpResponse(template.render(context, req))
 
