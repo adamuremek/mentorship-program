5a8dbba9972b19892b988d5be391a46831d9a74b
Anthony Peters <108294207+anthonyPeters143@users.noreply.github.com>
2024-02-24 19:04:13 -0500

    Updated sign-in-cards layouts to work on any sized screen and cleaned up main.css styles. Switched from using aspect-ratio to using min/max height stylings. Used media tags to dynamically change styling using screen width.
    Changed account_creation_0_mentor.html and  account_creation_3_mentor.html into  account_creation_sign_up_choices_mentor.html and  account_creation_experience_mentor.html
    Added styling for form element
    
    single_page_mentor.html:
    Upated to include account_creation_experience_mentor.html instead of account_creation_3_mentor.html
    Added styling for form element
    
    views.py:
    Updated register_mentor to use account_creation_sign_up_choices_mentor.html instead of account_creation_0_mentor.html
    Removed sign-in-card depreceated routes
    
    urls.py:
    Removed sign-in-card depreceated routes
    
    account_activion_mentor.html:
    Removed uneeded elements
    
    account_creation_user_agreement.html:
    Removed uneeded elements

--- mentorship_program_project/mentorship_program_app/urls.py
+++ mentorship_program_project/mentorship_program_app/urls.py
@@ -36,8 +36,6 @@ urlpatterns = [
     path('account_activation_mentee/', views.account_activation_mentee, name='account_activation_mentee'),
     path('account_activation_mentee_invalid/', views.account_activation_invalid_mentee, name='account_activation_invalid_mentee'),
     path('account_activation_mentee_valid/', views.account_activation_valid_mentee, name='account_activation_valid_mentee'),
-    path('account_creation_1_mentee/', views.account_creation_1_mentee, name='account_creation_1_mentee'),
-    path('account_creation_2_mentee/', views.account_creation_2_mentee, name='account_creation_2_mentee'),
     path('account_activation_mentor/', views.account_activation_mentor, name='account_activation_mentor'),
 
     #log in and out routes
--- mentorship_program_project/mentorship_program_app/views.py
+++ mentorship_program_project/mentorship_program_app/views.py
@@ -95,7 +95,7 @@ def register_mentee(req):
     return HttpResponse(template.render(context, req))
 
 def register_mentor(req):
-    template = loader.get_template('sign-in card/mentor/account_creation_0_mentor.html')
+    template = loader.get_template('sign-in card/mentor/account_creation_sign_up_choices_mentor.html')
     context = {}
     return HttpResponse(template.render(context, req))
 
@@ -161,88 +161,32 @@ def role_test(req):
 
 # TESTING AND DEV ROUTES WILL NEED TO CHECK/REVIEW BEFORE PUBLISHING
 def role_selection(request):
-    template = loader.get_template('sign-in card/role_selection.html')
+    template = loader.get_template('sign-in card/shared/role_selection.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
 
 def account_activation_mentee(request):
-    template = loader.get_template('sign-in card/account_activation_mentee.html')
+    template = loader.get_template('sign-in card/mentee/account_activation_mentee.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
 def account_activation_invalid_mentee(request):
-    template = loader.get_template('sign-in card/account_activation_invalid_mentee.html')
+    template = loader.get_template('sign-in card/mentee/account_activation_invalid_mentee.html')
     context = {
         'email':'demoemail@something.com'
     }
     return HttpResponse(template.render(context, request))
 
 def account_activation_valid_mentee(request):
-    template = loader.get_template('sign-in card/account_activation_valid_mentee.html')
+    template = loader.get_template('sign-in card/mentee/account_activation_valid_mentee.html')
     context = {
         'email':'demoemail@something.com'
     }
     return HttpResponse(template.render(context, request))
 
-def account_creation_1_mentee(request):
-    template = loader.get_template('sign-in card/account_creation_1_mentee.html')
-    context = {
-        'pronounlist': ['he', 'she', 'they'],
-    }
-    return HttpResponse(template.render(context, request))
-
-def account_creation_2_mentee(request):
-    template = loader.get_template('sign-in card/account_creation_2_mentee.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-def account_creation_3_mentee(request):
-    template = loader.get_template('sign-in card/account_creation_3_mentee.html')
-    context = {
-        'interestlist': [
-            'Artificial Intelligence', 
-            'Computer Graphics', 
-            'Data Structures & Algorithms',
-            'Networking',
-            'Operating Systems',
-            'Embedded Systems',
-            'Cloud Computing',
-            'Software Engineering',
-            'Distrubuted Systems',
-            'Game Development',
-            'Cybersecruity',
-            'System Analysis'],
-    }
-    return HttpResponse(template.render(context, request))
-
-def account_creation_4_mentee(request):
-    template = loader.get_template('sign-in card/account_creation_4_mentee.html')
-    context = {
-        'useragreement': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    }
-    return HttpResponse(template.render(context, request))
-
-
 def account_activation_mentor(request):
-    template = loader.get_template('sign-in card/account_activation_mentor.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-def account_creation_0_mentor(request):
-    template = loader.get_template('sign-in card/account_creation_0_mentor.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-def account_creation_1_mentor(request):
-    template = loader.get_template('sign-in card/account_creation_1_mentor.html')
-    context = {
-        'pronounlist': ['he', 'she', 'they'],
-    }
-    return HttpResponse(template.render(context, request))
-
-def account_creation_2_mentor(request):
-    template = loader.get_template('sign-in card/account_creation_2_mentor.html')
+    template = loader.get_template('sign-in card/mentor/account_activation_mentor.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
