cf9463843f6bbb1c219fec0b178362146f547938
AndrewPyscher <andypyscher@gmail.com>
2024-03-10 20:44:47 -0400

    small fixes
    with adam + david

---
+++ sql/reset_db.sql
@@ -0,0 +1,10 @@
+-- Step 1
+DROP DATABASE IF EXISTS mentorship_app;
+CREATE DATABASE mentorship_app;
+ALTER USER django_bot WITH PASSWORD 'temppass';
+
+-- Step 2: Need to be logged into me
+GRANT ALL ON SCHEMA public TO django_bot;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO django_bot;
+
+
