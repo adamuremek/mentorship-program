6ce434adf39e30c0ccf209fc16ce84010e603c3f
Justin-Goupil <142522764+goupil-justin@users.noreply.github.com>
2024-03-14 21:47:04 -0400

    Added Comments - Justin
    Added comments documenting the code.
    
    Added simple message to provide feedback that the operation was successful.

--- mentorship_program_project/mentorship_program_app/management/commands/create_super_admin.py
+++ mentorship_program_project/mentorship_program_app/management/commands/create_super_admin.py
@@ -5,10 +5,69 @@ from getpass import getpass
 
 
 class Command(BaseCommand):
-    help = "run this to create a new super admin account in the database"
+    """
+    Description
+    -----------
+    Custom django-admin command that creates a Super Administrator
+    and adds them to the database.
+
+    Properties
+    ----------
+    help = Displays string of information when
+           `python manage.py --help` is ran.
+
+    Instance Functions
+    -------------------
+    prompt_password_plain_text : Prompts the user to enter a password in plain text.
+    prompt_email : Prompts the user to enter an email.
+    handle : Executes the main body of code. Like main(String[] args) in Java. 
+
+    Static Functions
+    -------
+    (None)
+
+    Magic Functions
+    -------------
+     (None)
+
+    Authors
+    -------
+    David Kennamer ⌨️
+    Justin Goupil ♟️
+    """
+    help = "Run this to create a new super admin account in the database."
 
 
     def prompt_password_plain_text(self)->str:
+        """
+        Description
+        -----------
+        Prompts the user to enter a password.
+        Uses python getpass to hide user's input.
+
+        Parameters
+        ----------
+        self : Represents the instance of the class.
+
+        Returns
+        -------
+        - password (string) : Password stored as plain text.
+
+        Example Usage
+        -------------
+
+        >>> password = prompt_password_plain_text()
+        console view {
+            (admin password)>
+            (confirm admin password)>
+        }
+        password = 3x@mple_pas$w0rd_5
+
+        Authors
+        -------
+        David Kennamer ⌨️
+        Justin Goupil ♟️
+        """
         password = getpass('(admin password)> ')
         password2 = getpass('(confirm admin password)> ')
 
@@ -21,10 +80,73 @@ class Command(BaseCommand):
         return password
 
     def prompt_email(self)->str:
+        """
+        Description
+        -----------
+        Prompts the user to enter an email.
+
+        Parameters
+        ----------
+        self : Represents the instance of the class.
+
+        Returns
+        -------
+        - email (string) : email address.
+
+        Example Usage
+        -------------
+
+        >>> email = prompt_email()
+        console view {
+            (email)> jmgoupil@svsu.edu
+        }
+        email = jmgoupil@svsu.edu
+
+        Authors
+        -------
+        David Kennamer ⌨️
+        Justin Goupil ♟️
+        """
         return input("(email)> ")
 
     def handle(self, *args, **options):
+        """
+        Description
+        -----------
+        Prompts the user to enter an email.
+
+        Parameters
+        ----------
+        self : Represents the instance of the class.
+        *args : django method to pass arguments via command line.
+        **options : django method to pass optional arguments via 
+                    command line. Not used.
+
+        Returns
+        -------
+        - NONE
+
+        Example Usage
+        -------------
+
+        >>> handle()
+        console view {
+            (email)> sample@mail.com
+            (admin password)> 
+            (confirm admin password)>
+            Super Admin account has been created.
+            <End of Script>
+        }
+        A User and their relation in SuperAdminEntry is 
+        created and saved to the database.
+
+        Authors
+        -------
+        David Kennamer ⌨️
+        Justin Goupil ♟️
+        """
         admins = SuperAdminEntry.objects.all()
+
         if len(admins) > 0:
             print("[WARNING] there are already admins in the database, are you sure you wish to create an additional admin? ")
             ansr = input("(y/n)> ")
@@ -39,6 +161,8 @@ class Command(BaseCommand):
 
         super_admin_entry = SuperAdminEntry.objects.create(user_account=admin_user_account,bool_enabled=True)
         super_admin_entry.save()
+
+        print("Super Admin account has been created.")
 
 
 
