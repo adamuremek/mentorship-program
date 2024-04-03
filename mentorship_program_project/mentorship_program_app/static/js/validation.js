3dc0c99ed3a77687e30d5647234b3eee48d64541
Tanner Williams <tannerawilliams10@gmail.com>
2024-04-03 18:01:39 -0400

    small test

--- mentorship_program_project/mentorship_program_app/static/js/validation.js
+++ mentorship_program_project/mentorship_program_app/static/js/validation.js
@@ -230,8 +230,9 @@ document.addEventListener('DOMContentLoaded', winloaded => {
     // Assign event listener to each button
     for (button of buttons) {
         button.addEventListener('click', async e => {
+            
             let valid = await is_page_valid(cur_id + 1)
-
+            console.log(valid)
             if (!valid )
                 return
             snippets[cur_id].style = 'display: none;'
