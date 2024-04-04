eec380a857af33feed41d895dd27c6fd08d516ce
kotht <tjkoth@svsu.edu>
2024-04-04 02:49:08 -0400

    Logo Added & Other UI/UX Changes
    - Added the WINGS logo to the navigation bar.
    - Added the WINGS logo to the landing page sign in portal.
    - Updated navigation to include group labels to further define navigation.
    - Updated landing page styling to include logo.
    - Added password visibility icons to the landing page sign in form.
    - Limited and centered form when column layout is targeted. (UI ~ users will never have a pass greater than 36 chars, i.e. input field shouldn't be larger than this)
    - Added UX functionality to password visibility icons (can be enabled/disabled with enter, space, or click).
    - Added UX functionality to password reset text, following functionality described directly above.

--- mentorship_program_project/mentorship_program_app/static/js/landing_page/forgot_pass_modals.js
+++ mentorship_program_project/mentorship_program_app/static/js/landing_page/forgot_pass_modals.js
@@ -4,6 +4,7 @@ const exitButton = document.getElementById('modal-exit');
 const reset_email_field = document.getElementById('reset-email');
 const email_status_bad = document.getElementById('email-status-bad');
 
+// This routine when invoked assigns focus to the reset email element.
 function focusInput() {
     // Use setTimeout to delay the focus action ~ allow modal to show
     setTimeout(() => {
@@ -11,9 +12,23 @@ function focusInput() {
     }, 0); // Starting with 0 milliseconds
 }
 
-forgot_pass_btn.addEventListener("click", () => {
+// This function when invoked shows the forgotten password modal, and focuses
+// the input on the email input field. 
+function showModalAndFocusInput() {
     forgot_pass_modal_1.showModal();
     focusInput();
+}
+
+// Event listener for click event
+forgot_pass_btn.addEventListener("click", showModalAndFocusInput);
+
+// Event listener for keydown event
+forgot_pass_btn.addEventListener("keydown", (event) => {
+    // Check if Enter or Spacebar was pressed
+    if (event.key === 'Enter' || event.key === ' ' || event.key === 'Spacebar') {
+        event.preventDefault(); // Prevent the default action
+        showModalAndFocusInput();
+    }
 });
 
 async function closeModal() {
