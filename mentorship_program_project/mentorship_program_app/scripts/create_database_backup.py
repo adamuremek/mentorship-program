f3b427bc80be58e7c43307f7081b88d9a7f59f0a
Justin-Goupil <142522764+goupil-justin@users.noreply.github.com>
2024-04-10 00:23:55 -0400

    create_database_backup.py
    Now saves the file as .sql.tar.gz

--- mentorship_program_project/mentorship_program_app/scripts/create_database_backup.py
+++ mentorship_program_project/mentorship_program_app/scripts/create_database_backup.py
@@ -38,26 +38,38 @@ import subprocess
 import urllib.parse
 from datetime import datetime
 import os
+import tarfile
 
 def run():
+    #test()
+    #return 0
     try:
         str_time_stamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         backup_file = f"{os.environ.get('DB_NAME')}_{str_time_stamp}.sql"
         backup_file_path = f"{os.path.abspath(os.environ.get('DB_BACKUP_PATH'))}/{backup_file}"
+        tarball_path = f"{backup_file_path}.tar.gz"
 
         str_encoded_password = urllib.parse.quote_plus(os.environ.get("DB_PASSWORD"))
 
         command = [
             'pg_dump',
             f'--dbname=postgresql://{os.environ.get("DB_USER")}:{str_encoded_password}@{os.environ.get("DB_HOST")}:{os.environ.get("DB_PORT")}/{os.environ.get("DB_NAME")}',
-            f'--file={backup_file_path}'
+            f'--file={backup_file_path}',
+            f'--format=t'
         ]
 
         #Send it
         subprocess.run(command, check=True)
 
+        # Create tarball
+        with tarfile.open(tarball_path, 'w:gz') as tar:
+            tar.add(backup_file_path, arcname=os.path.basename(backup_file_path))
 
-        print(f"Backup created successfully: {os.path.abspath(backup_file_path)}")
+        #Remove the uncompressed file
+        os.remove(backup_file_path)
+
+
+        print(f"Backup created successfully: {os.path.abspath(os.path.abspath(tarball_path))}")
     except subprocess.CalledProcessError as e :
         print(f"Error: Failed to create backup - {e}")
     except Exception as e:
