1745ada7f8439f9dba62d0f2dbc2437edc95d753
ZippyFresh <loganzipp@yahoo.com>
2024-04-25 02:13:07 -0400

    lets try this and see how bad it breaks this time

--- mentorship_program_project/mentorship_program_app/routes/dashboard_routes.py
+++ mentorship_program_project/mentorship_program_app/routes/dashboard_routes.py
@@ -76,7 +76,7 @@ def dashboard(req):
         # mentee_account = session_user.mentee
         
         # mentee_account = session_user.objects.get(account_id=session_user.id)
-        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.mentee.account_id)
+        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.mentee.id)
         # requester_accounts = [user.requester for user in MentorshipRequest.objects.filter(requester=session_user.mentee.id)]
         # requester_accounts = [user.requester for user in MentorshipRequest.objects.filter(requester=mentee_account.id)]
 
@@ -121,7 +121,7 @@ def dashboard(req):
         
         # mentor_account = session_user.objects.get(account_id=session_user.id)
         # mentor_account = session_user.mentor
-        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.mentor.account_id)
+        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.mentor.id)
         # requester_accounts = [user.requester for user in MentorshipRequest.objects.filter(requester=session_user.mentor.id)]
 
         card_data = User.objects.filter(
@@ -190,6 +190,7 @@ def dashboard(req):
             "session_user"      : session_user,
             "role"              : role,
             "THE_BIG_LIST"      : requester_accounts,
+            "experiment"        : requests
     }
     render = template.render(context, req)
     return HttpResponse(render)
