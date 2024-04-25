3d76c6336b85d5658eddb85227bdb1b0cc8672e6
ZippyFresh <loganzipp@yahoo.com>
2024-04-24 22:48:40 -0400

    hahahahahah

--- mentorship_program_project/mentorship_program_app/routes/dashboard_routes.py
+++ mentorship_program_project/mentorship_program_app/routes/dashboard_routes.py
@@ -76,7 +76,7 @@ def dashboard(req):
         # mentee_account = session_user.mentee
         
         # mentee_account = session_user.objects.get(account_id=session_user.id)
-        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.mentee.id)
+        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.id)
         # requester_accounts = [user.requester for user in MentorshipRequest.objects.filter(requester=session_user.mentee.id)]
         # requester_accounts = [user.requester for user in MentorshipRequest.objects.filter(requester=mentee_account.id)]
 
@@ -121,7 +121,7 @@ def dashboard(req):
         
         # mentor_account = session_user.objects.get(account_id=session_user.id)
         # mentor_account = session_user.mentor
-        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.mentor.id)
+        requester_accounts = MentorshipRequest.objects.filter(requester=session_user.id)
         # requester_accounts = [user.requester for user in MentorshipRequest.objects.filter(requester=session_user.mentor.id)]
 
         card_data = User.objects.filter(
